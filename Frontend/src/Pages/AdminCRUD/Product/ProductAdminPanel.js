1238180688fcaaa588508ad9fd4a8abe12c55887
Tadas <tadastadas81@gmail.com>
2023-04-29 22:04:41 +0300

    Products mui  admin panel

--- Frontend/src/Pages/AdminCRUD/Product/ProductAdminPanel.js
+++ Frontend/src/Pages/AdminCRUD/Product/ProductAdminPanel.js
@@ -6,18 +6,12 @@ import { DataGrid,
   GridToolbarFilterButton,
   GridToolbarExport,
   GridToolbarDensitySelector } from '@mui/x-data-grid';
-
-  import { DataGridPro,GridToolbarContainer,
-    GridToolbarColumnsButton,
-    GridToolbarFilterButton,
-    GridToolbarExport,
-    GridToolbarDensitySelector } from '@mui/x-data-grid-pro';
-
+  
+  
 
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import { IconButton } from "@material-ui/core";
-
 import {auth} from '../../../firebase_config'
 
 //https://www.npmjs.com/package/react-player
@@ -64,6 +58,7 @@ const ProductAdminPanel = () => {
     // //------------------------------------------
 
 
+    
 
     //delete not always doing its delete thing
     const handleDeleteProduct=(product)=>{
@@ -81,7 +76,7 @@ const ProductAdminPanel = () => {
             })
             .then(response => response.json())
             .then((usefulData) => {
-                //console.log(usefulData);
+                console.log(usefulData);
                 setLoading(false);
                 setData(usefulData);
                 setRefresh(true);
@@ -99,36 +94,76 @@ const ProductAdminPanel = () => {
 
     var columns = [
       
-      { field: 'id', headerName: 'Count', width: 120 },
-      { field: 'uid', headerName: 'Firebase uid', width: 210 },
-      { field: 'name', headerName: 'Product name', width: 200 },
+      { field: 'id', headerName: 'Count', width: 80, cellClassName: 'vertical-line' },
+      { field: 'uid', headerName: 'Firebase uid', width: 210, cellClassName: 'vertical-line' },
+      { field: 'name', headerName: 'Product name', width: 200, cellClassName: 'vertical-line' },
+
       { 
         field: 'price', 
         headerName: 'Price', 
         type: 'number', 
-        width: 200,
-        valueFormatter: ({ value }) => `${value} €` 
+        width: 120,
+        valueFormatter: ({ value }) => `${value} €`,
+        cellClassName: 'vertical-line'
       },
-      { field: 'desc', headerName: 'Description',  width: 200},
-      { field: 'type', headerName: 'Product type', width: 200},
-      { field: 'image', headerName: 'Image', width: 200},
-      { field: 'audio', headerName: 'Audio', width: 200},
+
+      { field: 'desc', headerName: 'Description',  width: 200, cellClassName: 'vertical-line'},
+      { field: 'type', headerName: 'Product type', width: 200, cellClassName: 'vertical-line'},
+      { 
+        field: 'image', 
+        headerName: 'Image', 
+        width: 70, 
+        cellClassName: 'vertical-line',
+        renderCell: (params) => (
+          
+          <a href={params.value}>
+            <img src={params.value} width={40} height={40}></img>
+          </a>
+        ),
+      },
+      {
+        field: 'audio', 
+        headerName: 'Audio', 
+        width: 350, 
+        cellClassName: 'vertical-line',
+        renderCell: (params) => {
+          if (params.value != "None") {
+            return (
+              <ReactPlayer
+                url={params.value}
+                width="100%"
+                height="50px"
+                playing={false}
+                controls={true}
+              />
+            );
+          } else {
+            return (
+              <>
+                None
+              </>
+            );
+          }
+        },
+      },
       {
         field: 'actions',
         headerName: 'Actions',
         width: 150,
+        
         renderCell: (params) => {
           return (
             <div>
               <Link to={`/editproduct/${params.row.uid}`}>
               <Button>Edit</Button>
               </Link>
-              {console.log(params)}
+
               {/* params.row gives the object */}
               <Button onClick={() => handleDeleteProduct(params.row)}>Delete</Button>
             </div>
           );
         },
+        
       },
       
       
@@ -154,18 +189,19 @@ console.log(displayData);
 
     return(
         <div style={{ color: 'white'}}>
-
+{/* 
             <Link to='/createproduct'>
                 <Button>Add product</Button>
-            </Link>
+            </Link> */}
             {/* when empty this will get stuck on loading. */}
+            <h1>Products</h1>
             {loading ?(
                 <p>Loading...</p>
             ) : error ? (
                 <p>An error occured</p>
             ):(
             <>
-              <div style={{ height: 600, width: '100%', background: 'rgba(255, 255, 255, 1)'}}>
+              <div style={{ height: '100%', width: '100%', background: 'rgba(255, 255, 255, 1)'}}>
                 <DataGrid style={{ }}
                    checkboxSelection={false}
                   rows={displayData}
